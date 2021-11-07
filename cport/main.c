c60bbc6247f75d768057e608265f45b21bd3aa5c
krishpranav <krisna.pranav@gmail.com>
2021-11-07 17:09:11 +0530

    cport

---
+++ cport/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  cport
+//
+//  Created by Apple on 07/11/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
