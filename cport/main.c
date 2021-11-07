226372ee21bbf198aa3d27d0f157ae1f55a7f9d4
krishpranav <krisna.pranav@gmail.com>
2021-11-07 17:12:12 +0530

    cport

--- cport/main.c
+++ cport/main.c
@@ -1,14 +1,21 @@
-//
-//  main.c
-//  cport
-//
-//  Created by Apple on 07/11/21.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main(int argc, char **argv)
+{
+    struct hostent *host;
+    int err, i, sock, start, end;
+    char hostname[100];
+    struct sockaddr_in sa;
+    
+    printf("Enter hostname or ip: ");
+    gets(hostname);
+    
+    printf("\nEnter first portnumber: ");
+    scanf("%d", &start);
+    
+    printf("\nEnter end port number: ");
+    scanf("%d", &end);
+    
+    strncpy((char*) &sa, "", sizeof sa);
+    sa.sin_family = AF_INET;
 }
